87bc7fcd51e2699a07e0e1f8f822e0b5a4249817
jon <jon.gao.yang@gmail.com>
2025-08-25 00:50:40 -0400

    started pytorch mnist digit recognizer program

--- 0 - mnist/mnist_tinygrad.py
+++ 0 - mnist/mnist_tinygrad.py
@@ -1,27 +1,28 @@
-# based on https://github.com/pytorch/examples/blob/main/mnist/main.py
-import numpy as np, matplotlib.pyplot as plt, time, os, torch
+# model based off tinygrad/examples/beautiful_mnist.py
+from typing import List, Callable
+from tinygrad import Tensor, TinyJit, nn, GlobalCounters
+from tinygrad.helpers import getenv, colored, trange
+import numpy as np, matplotlib.pyplot as plt, time, os
 
 ###### [ 1/3 : MODEL INITIALIZATION ] ######
 
-class Model(torch.nn.Module):
-    def __init__(self):
-        super(Model, self).__init__()
-        self.flatten = torch.nn.Flatten()
-        self.linear_relu_stack = torch.nn.Sequential(
-            torch.nn.Linear(28*28, 512),
-            torch.nn.ReLU(),
-            torch.nn.Linear(512, 512),
-            torch.nn.ReLU(),
-            torch.nn.Linear(512, 10),
-        )
+class Model:
+  def __init__(self):
+    self.layers: List[Callable[[Tensor], Tensor]] = [
+      nn.Conv2d(1, 32, 5), Tensor.relu,
+      nn.Conv2d(32, 32, 5), Tensor.relu,
+      nn.BatchNorm(32), Tensor.max_pool2d,
+      nn.Conv2d(32, 64, 3), Tensor.relu,
+      nn.Conv2d(64, 64, 3), Tensor.relu,
+      nn.BatchNorm(64), Tensor.max_pool2d,
+      lambda x: x.flatten(1), nn.Linear(576, 10)]
 
-    def forward(self, x:torch.Tensor):
-        x = self.flatten(x)
-        return self.linear_relu_stack(x)
-
+  def __call__(self, x:Tensor) -> Tensor: return x.sequential(self.layers)
+    
 ###### [ 2/3 : HELPER FUNCTIONS ] ######
 
 # based on code from Andrew Ng's "Advanced Learning Algorithms" Coursera course
+@Tensor.test()
 def plot_kaggle_data(X, y, model, predict=False):
     fig, axes = plt.subplots(8,8, figsize=(5,5))
     fig.tight_layout(pad=0.13,rect=[0, 0.03, 1, 0.91]) #[left, bottom, right, top]
@@ -29,7 +30,7 @@ def plot_kaggle_data(X, y, model, predict=False):
     for i,ax in enumerate(axes.flat):
 
         # Select random image
-        random_index = torch.randint(low=0, high=X.shape[0], size = ())
+        random_index = Tensor.randint(high=X.shape[0])
         
         # Display random image
         ax.imshow(X[random_index].numpy().reshape(28, 28), cmap='gray')
@@ -45,12 +46,13 @@ def plot_kaggle_data(X, y, model, predict=False):
     fig.suptitle("Label, yhat", fontsize=14)
     plt.show()
 
+@Tensor.test()
 def write_kaggle_submission(model):
     print('BEGINNING TEST SET INFERENCE')
 
     data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'datasets/digit-recognizer/test.csv')
     X = np.loadtxt(data_dir, dtype = int, delimiter = ',', skiprows = 1) # data loading
-    X = torch.Tensor(X).reshape(-1, 1, 28, 28)
+    X = Tensor(X).reshape(-1, 1, 28, 28)
 
     out = np.concatenate((np.arange(1, X.shape[0]+1).reshape((-1, 1)), model(X).argmax(axis=1).reshape((-1, 1)).numpy()), axis = 1)
     data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'datasets/digit-recognizer/submission.csv')
@@ -58,37 +60,40 @@ def write_kaggle_submission(model):
 
     print('TEST SET INFERENCE COMPLETE')
 
+@TinyJit
+@Tensor.train()
+def train_step() -> Tensor:
+    samples = Tensor.randint(batch_size, high=X.shape[0])
+    optim.zero_grad()
+    loss = model(X[samples]).sparse_categorical_crossentropy(y[samples]).backward()
+    optim.step()
+    return loss
+
+@TinyJit
+@Tensor.test()
+def eval() -> Tensor: return (model(X[:batch_size]).argmax(axis=1) == y[:batch_size]).mean()
+
 ###### [ 3/3 : MAIN FUNCTION ] ######
 
+# hyperparameters
 model = Model()
-steps = 1000
-optimizer = torch.optim.AdamW(model.parameters())
-lossFunc = torch.nn.CrossEntropyLoss()
+steps = 70
+batch_size = 512
+optim = nn.optim.Adam(nn.state.get_parameters(model))
+print('TRAINING BEGINS (with', sum([t.numel() for t in nn.state.get_parameters(model)]), 'parameters)')
 
 # loading data from file, then splitting into labels (first col) and pixel vals
 data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'datasets/digit-recognizer/train.csv')
 [y, X] = np.split(np.loadtxt(data_dir, dtype = int, delimiter = ',', skiprows = 1), [1], axis = 1)
-y, X = torch.Tensor(np.squeeze(y)), torch.Tensor(X).reshape(-1, 1, 28, 28)
+y, X = Tensor(np.squeeze(y)), Tensor(X).reshape(-1, 1, 28, 28)
 
-X = (X - torch.mean(X))/torch.std(X) # data normalization
-print('TRAINING BEGINS (with', sum(p.numel() for p in model.parameters()), 'parameters)')
-startTime = time.time()
-
-# optimization
-for k in range(steps):
-
-    randint = torch.randint(low = 0, high = X.shape[0], size = (1,)).item()
-    
-    # forward
-    total_loss = lossFunc(model.forward(X[randint]), y[randint].reshape((1,)).to(dtype=torch.long))
-
-    # backward
-    optimizer.zero_grad()
-    total_loss.backward()
-    
-    # update parameters
-    optimizer.step()
+# training loop and occasional eval
+acc, startTime = float('nan'), time.time()
+for k in (t:=trange(steps)):
+    loss = train_step()
+    if k % 10 == 9: acc = eval().item()*100
+    t.set_description(f"loss: {loss.item():6.2f} accuracy: {acc:5.2f}%")
 
 print('TRAINING COMPLETE (in', time.time() - startTime, 'sec)')
 plot_kaggle_data(X, y, model, predict = True) # model demo
-# write_kaggle_submission(model) # model usage
+write_kaggle_submission(model) # model usage
